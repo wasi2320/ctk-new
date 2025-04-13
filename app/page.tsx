00f725a901a21ec378c6274b718da5188ccf8acd
yasiruaf <muhammadyasir4741@gmail.com>
2025-04-13 22:17:41 +0500

    build fix

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import TechStackSection from "./components/sections/TechStackSection";
 // import BusinessCarouselSection from "./components/sections/BusinessCarouselSection";
 import TechCarouselSection from "./components/sections/TechCarouselSection";
 import EnsureSection from "./components/sections/EnsureSection";
-import ConnectSection from "./components/sections/ConnectSection";
 
 export default function Home() {
   return (
