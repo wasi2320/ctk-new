1528a240a5e00293aa17bd4cb31f6be01e106cc4
Usama Aamir {Feline Predator} <87513168+usama7365@users.noreply.github.com>
2025-05-19 21:48:19 +0500

    feat: update ClutchWidget component with new widget configuration and script integration
    - Integrated Clutch widget script for improved functionality.
    - Updated widget attributes for enhanced display and responsiveness.
    - Adjusted data attributes to reflect the latest configuration and design requirements.

--- app/components/ClutchWidget.tsx
+++ app/components/ClutchWidget.tsx
@@ -1,22 +1,29 @@
 "use client";
+import Script from "next/script";
 
 const ClutchWidget = () => {
   return (
-    <div className="w-full align-center justify-center flex overflow-hidden">
-      <div
-        className="clutch-widget w-[880px] self-center"
-        data-url="https://widget.clutch.co"
-        data-widget-type="4"
-        data-darkbg="1"
-        data-height="auto"
-        data-width="800px"
-        data-nofollow="true"
-        data-expandifr="true"
-        data-scale="100"
-        data-primary-color="#000000"
-        data-clutchcompany-id="2466293"
+    <>
+      <Script
+        src="https://widget.clutch.co/static/js/widget.js"
+        strategy="afterInteractive"
       />
-    </div>
+      <div className="w-full align-center justify-center flex overflow-hidden">
+        <div
+          className="clutch-widget"
+          data-url="https://widget.clutch.co/"
+          data-widget-type="12"
+          data-height="375"
+          data-nofollow="true"
+          data-expandifr="true"
+          data-scale="100"
+          data-primary-color="#082105"
+          data-secondary-color="#082105"
+          data-reviews="375170,373193,371564,365725,364927,362840"
+          data-clutchcompany-id="2466293"
+        />
+      </div>
+    </>
   );
 };
 
