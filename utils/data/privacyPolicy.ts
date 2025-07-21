5d714a3f03b3f38c67739e82fe2223c9fcc20ee1
wHat0 <mapk03@hotmail.com>
2025-07-22 02:11:29 +0500

    fix: correct branding references from CodeToCloud to CodeToKloud in About Us and Privacy Policy sections
    - Updated the company name in the About Us page and Privacy Policy to ensure consistency and accuracy across the application.
    - This change enhances brand identity and aligns with the recent rebranding efforts.

--- utils/data/privacyPolicy.ts
+++ utils/data/privacyPolicy.ts
@@ -3,7 +3,7 @@ export const privacyPolicyData = {
   sections: [
     {
       heading: "Introduction",
-      content: `The website ("Site") for CodeToCloud ("CodeToCloud", "We", "Our", "Us" or "Company") was created to provide cloud solutions, software development services, and other related services (the "Services"). The privacy of our Site visitors ("you," "your," or "user") is important to us, and in order to protect your personal information, we have implemented the following Privacy Policy with provisions that apply to the collection of data by CodeToCloud, its subsidiaries, and its affiliates.Our Privacy Policy discloses the type and nature of information we collect and how we use it, as well as the choices you can make about the way your information is collected and used. We also explain how any requests for personal or personally identifiable information will be used. By exploring and using CodeToCloud's Site and/or Services, and by submitting information to CodeToCloud, you signify acceptance to the terms of our Privacy Policy.If you have questions or concerns regarding this statement, you should first contact us at info@codetocloud.com.`,
+      content: `The website ("Site") for CodeToKloud ("CodeToKloud", "We", "Our", "Us" or "Company") was created to provide cloud solutions, software development services, and other related services (the "Services"). The privacy of our Site visitors ("you," "your," or "user") is important to us, and in order to protect your personal information, we have implemented the following Privacy Policy with provisions that apply to the collection of data by CodeToKloud, its subsidiaries, and its affiliates.Our Privacy Policy discloses the type and nature of information we collect and how we use it, as well as the choices you can make about the way your information is collected and used. We also explain how any requests for personal or personally identifiable information will be used. By exploring and using CodeToKloud's Site and/or Services, and by submitting information to CodeToKloud, you signify acceptance to the terms of our Privacy Policy.If you have questions or concerns regarding this statement, you should first contact us at info@codetokloud.com.`,
     },
     {
       heading: "Updates and Changes to the Privacy Policy",
@@ -13,7 +13,7 @@ Please revisit this page to familiarize yourself with changes to the Privacy Pol
     },
     {
       heading: "Information We Collect",
-      content: `We are required by law, regulation, or securities reasons to ask for certain personal information in order to provide our Services to you. CodeToCloud makes commercially reasonable best efforts to maintain the confidentiality, integrity, and security of our clients' personal information.
+      content: `We are required by law, regulation, or securities reasons to ask for certain personal information in order to provide our Services to you. CodeToKloud makes commercially reasonable best efforts to maintain the confidentiality, integrity, and security of our clients' personal information.
 
 You may choose to provide additional information during subsequent visits to the Site, but keep in mind that some of this information will be required if you wish to partake in the Services.
 
@@ -28,7 +28,7 @@ When visiting our website, we also store every instance of access in a log file 
 • Date and time of access
 • IP address of the accessing computer
 
-Keeping client information secure, and using it only as our Users want us to, are matters of principle for all of us at CodeToCloud. With this in mind, here is our commitment to each and every User:
+Keeping client information secure, and using it only as our Users want us to, are matters of principle for all of us at CodeToKloud. With this in mind, here is our commitment to each and every User:
 
 • Except as you may otherwise expressly approve, we will limit the collection and use of client and user information to what we believe would be useful to service your accounts, administer our business, or to tell you about our Services;
 • We will restrict employee access to client and user information to those who need to know in order to provide Services to you;
@@ -36,15 +36,15 @@ Keeping client information secure, and using it only as our Users want us to, ar
 • We will maintain commercially reasonable and customary security standards and procedures to protect information about you; and
 • We will respond quickly to your request to correct inaccurate information.
 
-CodeToCloud will also gather the names and email addresses of users who contact us through the Site with questions about our company or services. We collect this information for the sole purpose of responding to such inquiries and do not store the contact information unless requested to do so, as in the case of job applicants who submit resumes.
+CodeToKloud will also gather the names and email addresses of users who contact us through the Site with questions about our company or services. We collect this information for the sole purpose of responding to such inquiries and do not store the contact information unless requested to do so, as in the case of job applicants who submit resumes.
 
-Any data including, but not limited to, your name, address, telephone number, e-mail address, and biometric information submitted to CodeToCloud during identity verification process is handled securely by our third-party, specialist identity verification provider. CodeToCloud retains an ID and status of the process for our information and does NOT store, or use, any of the personally identifiable information (PII) submitted as part of that process.`,
+Any data including, but not limited to, your name, address, telephone number, e-mail address, and biometric information submitted to CodeToKloud during identity verification process is handled securely by our third-party, specialist identity verification provider. CodeToKloud retains an ID and status of the process for our information and does NOT store, or use, any of the personally identifiable information (PII) submitted as part of that process.`,
     },
     {
       heading: "How and Why We Gather Information",
       content: `When you register for an account for the Services or at any later time, we collect certain personal information from you to open an account, transact business, communicate with you, verify your identity and fulfill legal and regulatory requirements. We call this your "Profile." From time to time, we may request additional information (e.g., through surveys) to help us further assess your needs and preferences.
 
-If you choose to provide such information, during registration or otherwise, you are giving CodeToCloud the permission to use and store it consistent with this Privacy Policy. We may also obtain your personal information from your transactions with us or other Users through the Services, or from third parties such as credit reporting agencies. If we combine or associate information from other sources with personal information that you provide directly to us through or in connection with the Services, we will treat the combined information as personal information in accordance with this Privacy Policy.`,
+If you choose to provide such information, during registration or otherwise, you are giving CodeToKloud the permission to use and store it consistent with this Privacy Policy. We may also obtain your personal information from your transactions with us or other Users through the Services, or from third parties such as credit reporting agencies. If we combine or associate information from other sources with personal information that you provide directly to us through or in connection with the Services, we will treat the combined information as personal information in accordance with this Privacy Policy.`,
     },
     {
       heading: "Cookies",
@@ -62,11 +62,11 @@ We or our service providers may also collect web surfing data related to your us
     },
     {
       heading: "Contact Information",
-      content: `Please contact CodeToCloud with any questions or comments about this Privacy Policy, your personal information, or your choices with regards to our collection and use of your personal information, email at info@codetocloud.com.
+      content: `Please contact CodeToKloud with any questions or comments about this Privacy Policy, your personal information, or your choices with regards to our collection and use of your personal information, email at info@codetokloud.com.
 
 If you believe that we have infringed your rights, we encourage you to contact us first so that we can try to resolve the issue informally.
 
-For citizens of the European Union and United States, please contact info@codetocloud.com for requests and inquiries regarding the data retained and managed by this portal and your rights to be forgotten related to the General Data Protection Regulation (GDPR).`,
+For citizens of the European Union and United States, please contact info@codetokloud.com for requests and inquiries regarding the data retained and managed by this portal and your rights to be forgotten related to the General Data Protection Regulation (GDPR).`,
     },
   ],
 };
